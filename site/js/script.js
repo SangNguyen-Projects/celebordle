ed91e92a6881b78018d1c59d8ff705c0754ac7d5
SangNguyen-Projects <sangnguyen403213@gmail.com>
2022-07-29 18:30:32 -0700

    celebordle

--- site/js/script.js
+++ site/js/script.js
@@ -1,9 +1,44 @@
-var maxHeight = 200;
+var maxHeight = 150;
 
+const arr = ['dwayne johnson', 'justin bieber', 'robert downey jr'];
+var initialRandomInt = Math.floor(Math.random() * arr.length);
+
+var checkCelebrity = arr[initialRandomInt];
+
+function firstImage(){
+	var img = "<img id = 'image1' src = 'images/";
+	img += checkCelebrity;
+	img += ".jpg' alt = ''/>"
+	document.write(img);
+}
 function input() {
-	var x = document.getElementById('firstLine').value;
-	if(x != "Justin Bieber" && x != "justin bieber"){
+	var x = document.getElementById('firstLine').value.toLowerCase();
+	if(x != checkCelebrity){
 		maxHeight += 100;
-		document.getElementById('image1').style.maxHeight = maxHeight + "px";
+		if(maxHeight <= 650){
+			document.getElementById('image1').style.maxHeight = maxHeight + "px";
+		}
+	}else{
+		var celebrityCapitalized = checkCelebrity.charAt(0).toUpperCase();
+		for(let i = 1; i < checkCelebrity.length; i++){
+			if(checkCelebrity.charAt(i-1) == " "){
+				celebrityCapitalized += checkCelebrity.charAt(i).toUpperCase();
+			}else{
+				celebrityCapitalized += checkCelebrity.charAt(i);
+			}
+		}
+		document.getElementById('image1').style.maxHeight = "600px";
+		alert("Correct! The celebrity is " + celebrityCapitalized);
 	}
+}
+
+function changeCelebrity() {
+	var randomInt = Math.floor(Math.random() * arr.length);
+	var newCelebrity = arr[randomInt];
+
+	document.getElementById('image1').src = "images/" + newCelebrity + ".jpg";
+	checkCelebrity = newCelebrity
+
+	maxHeight = 150;
+	document.getElementById('image1').style.maxHeight = "150px";
 }
