eede9ff40d8fab102d01656e3a0158b328837aea
SangNguyen-Projects <sangnguyen403213@gmail.com>
2022-07-31 21:12:15 -0700

    celebordle

--- site/js/script.js
+++ site/js/script.js
@@ -1,6 +1,7 @@
 var maxHeight = 150;
 
-const arr = ['dwayne johnson', 'justin bieber', 'robert downey jr'];
+const arr = ['chris evans', 'dwayne johnson', 'justin bieber', 'robert downey jr', 
+'tom holland'];
 var initialRandomInt = Math.floor(Math.random() * arr.length);
 
 var checkCelebrity = arr[initialRandomInt];
@@ -11,7 +12,8 @@ function firstImage(){
 	img += ".jpg' alt = ''/>"
 	document.write(img);
 }
-function input() {
+
+function checkInput() {
 	var x = document.getElementById('firstLine').value.toLowerCase();
 	if(x != checkCelebrity){
 		maxHeight += 100;
@@ -19,19 +21,44 @@ function input() {
 			document.getElementById('image1').style.maxHeight = maxHeight + "px";
 		}
 	}else{
-		var celebrityCapitalized = checkCelebrity.charAt(0).toUpperCase();
-		for(let i = 1; i < checkCelebrity.length; i++){
-			if(checkCelebrity.charAt(i-1) == " "){
-				celebrityCapitalized += checkCelebrity.charAt(i).toUpperCase();
-			}else{
-				celebrityCapitalized += checkCelebrity.charAt(i);
-			}
-		}
 		document.getElementById('image1').style.maxHeight = "550px";
-		alert("Correct! The celebrity is " + celebrityCapitalized);
+		alert("Correct! The celebrity is " + capitalize(checkCelebrity));
 	}
 }
 
+function enterKey(event){
+	if(event.key === "Enter"){
+		event.preventDefault();
+		checkInput();
+	}
+}
+
+function preventDefault(event){
+	if(event.key === "Enter"){
+		event.preventDefault();
+	}
+}
+
+function capitalize(name){
+	var capitalized = name.charAt(0).toUpperCase();
+	for(let i = 1; i < name.length; i++){
+			if(name.charAt(i-1) == " "){
+				capitalized += name.charAt(i).toUpperCase();
+			}else{
+				capitalized += name.charAt(i);
+			}
+		}
+	return capitalized;
+}
+
+function celebrityOptions(){
+	for(let i = 0; i < arr.length; i++){
+		var option = "<option value = '";
+		option += capitalize(arr[i]);
+		option += "'>";
+		document.write(option);
+	}
+}
 function changeCelebrity() {
 	var randomInt = Math.floor(Math.random() * arr.length);
 	var newCelebrity = arr[randomInt];
